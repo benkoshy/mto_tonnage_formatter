9916a41b83d7f2c1a8daaac10f0ebb60411b59d3
Ben Koshy <benkoshy@hotmail.com>
2026-03-06 15:18:49 +1100

    finalise: formatter
    add: ability to add overall description
    
    WIP: add rows
    
    WIP: halfway through adding a row and column
    
    add: rows
    
    WIP: formatter running
    
    fix: add validation for mto formatter
    
    add: tek1 favico

--- src/Main.elm
+++ src/Main.elm
@@ -1,22 +1,26 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img, form, label, input, p)
-import Html.Attributes exposing (src, class)
+import Html exposing (Html, div, form, h1, img, input, label, p, small, text)
+import Html.Attributes exposing (class, src, value, classList)
 import Html.Events exposing (onInput)
 import Round exposing (round)
+
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {tonnage: Float}
+    { tonnage : Float
+    , overall : String
+    , link : String
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {tonnage = 0 }, Cmd.none )
+    ( { tonnage = 0, overall = "overall", link = "" }, Cmd.none )
 
 
 
@@ -26,15 +30,24 @@ init =
 type Msg
     = NoOp
     | Tonnage String
+    | Overall String
+    | Link String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of 
-        NoOp
-          -> ( model, Cmd.none )
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
         Tonnage tonnes ->
-            (  {model | tonnage = (Result.withDefault 0 ( String.toFloat tonnes |> Result.fromMaybe "invalid")) }, Cmd.none )
+            ( { model | tonnage = Result.withDefault 0 (String.toFloat tonnes |> Result.fromMaybe "invalid") }, Cmd.none )
+
+        Overall overall ->
+            ( { model | overall = overall }, Cmd.none )
+
+        Link link ->
+            ( { model | link = link }, Cmd.none )
 
 
 
@@ -44,17 +57,44 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ form [ src "/logo.svg" ] [
-          div [ class "mb-3"] [
-                label [ class "form-label" ]  [ text "Tonnage"  ],
-                input [ class "form-control", onInput Tonnage] [ ]                
-          ]
-        ],
-    div [] [
-            p [] [text ("Tonnes " ++ (Round.round 2 model.tonnage))]
-            ]        
+        [ h1 [] [ text "Material Take Off Formatter" ]
+        , form [ src "/logo.svg" ]
+            [ div [ class "my-5 row" ]
+                [ div [ class "col-4" ] [ label [ class "form-label" ] [ text "Tonnage" ] ]
+                , div [ class "col-8" ] [ input [ class "form-control", onInput Tonnage ] [] ]
+                ]
+            , div [ class "my-5 row" ]
+                [ div [ class "col-4" ] [ label [ class "form-label" ] [ text "Overall" ] ]
+                , div [ class "col-8" ]
+                    [ input [ class "form-control", value model.overall, onInput Overall ] []
+                    , p [] [ small [ class "text-muted" ] [ text "Tonnages for what?  Usually it's overall, but for certain items you might say: e.g. 'Building A'" ] ]
+                    ]
+                ]
+            , div [ class "my-5 row" ]
+                [ div [ class "col-4" ] [ label [ class "form-label" ] [ text "Link" ] ]
+                , div [ class "col-8" ]
+                    [ input [ class "form-control", onInput Link ] []
+                    , p [] [ small [ class "text-muted" ] [ text "Carefully paste the Google Drive link" ] ]
+                    ]
+                ]
+            ]
+        , div [ class "my-5 row" ]
+            [ input [ classList [("form-control", True), (isValid model, True)] , value (formatResult model) ] [] 
+            , p [] [ small [ class "text-muted" ] [ text "CTRL + A (to select all) and then CTRL + C (to copy)" ] ]
+            ]
         ]
 
+
+formatResult : Model -> String
+formatResult model =
+    "Tonnes: " ++ Round.round 2 model.tonnage ++ " (" ++ model.overall ++ ")" ++ ", Link: " ++ model.link
+
+isValid : Model -> String
+isValid model =
+    if model.tonnage > 0 && (not (String.isEmpty (model.overall))) && (not (String.isEmpty (model.link)))  then "is-valid"
+    else 
+        "is-invalid"
+    
 
 
 ---- PROGRAM ----
@@ -68,4 +108,3 @@ main =
         , update = update
         , subscriptions = always Sub.none
         }
-
